89c504daad0de13a873244c04662c6fa4cf5947a
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-05-07 09:05:36 +1000

    Fix path

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,5 @@ export DisableArcade=1
 dotnet pack $SRC_DIR/src/dotnet-interactive/dotnet-interactive.csproj
 dotnet tool install --add-source $SRC_DIR/src/dotnet-interactive/bin/Debug --tool-path $DOTNET_TOOLS Microsoft.dotnet-interactive
 
-mkdir "$PREFIX\share\jupyter"
-cp -Rv "$RECIPE_DIR\kernels" "$PREFIX\share\jupyter\kernels"
+mkdir "$PREFIX/share/jupyter"
+cp -Rv "$RECIPE_DIR/kernels" "$PREFIX/share/jupyter/kernels"
