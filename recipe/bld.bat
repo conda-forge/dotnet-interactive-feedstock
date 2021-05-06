b503f8794e28472596fa043dc1de392e60dab535
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-05-07 09:52:34 +1000

    Specify net5.0 framework; update Windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,8 @@ setlocal enabledelayedexpansion
 
 set DisableArcade=1
 
-dotnet pack %SRC_DIR%/src/dotnet-interactive/dotnet-interactive.csproj
-dotnet tool install --add-source %SRC_DIR%/src/dotnet-interactive/bin/X64/Debug --tool-path %DOTNET_TOOLS% Microsoft.dotnet-interactive
+dotnet pack  --framework net5.0 --configuration Release --runtime win-x64 %SRC_DIR%/src/dotnet-interactive/dotnet-interactive.csproj
+dotnet tool install --add-source %SRC_DIR%/src/dotnet-interactive/bin/X64/Release --tool-path %DOTNET_TOOLS% Microsoft.dotnet-interactive
 
 mkdir "%PREFIX%\share\jupyter"
 xcopy "%RECIPE_DIR%\kernels" "%PREFIX%\share\jupyter\kernels" /E /I /F /Y
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 export DisableArcade=1
 
-dotnet pack --configuration Release --runtime linux-x64 $SRC_DIR/src/dotnet-interactive/dotnet-interactive.csproj
+dotnet pack --framework net5.0 --configuration Release --runtime linux-x64 $SRC_DIR/src/dotnet-interactive/dotnet-interactive.csproj
 dotnet tool install --add-source $SRC_DIR/src/dotnet-interactive/bin/Release --tool-path $DOTNET_TOOLS Microsoft.dotnet-interactive
 
 mkdir "$PREFIX/share/jupyter"
