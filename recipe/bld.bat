d94120dce5f8a07b1b33b63689d6e5938982bcda
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-05-06 21:53:49 +1000

    wip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 set DisableArcade=1
 
 dotnet pack %SRC_DIR%/src/dotnet-interactive/dotnet-interactive.csproj
-dotnet tool install --add-source %SRC_DIR%/src/dotnet-interactive/bin/Debug --tool-path %DOTNET_TOOLS% Microsoft.dotnet-interactive
+dotnet tool install --add-source %SRC_DIR%/src/dotnet-interactive/bin/X64/Debug --tool-path %DOTNET_TOOLS% Microsoft.dotnet-interactive
 
 mkdir "%PREFIX%\share\jupyter"
 xcopy "%RECIPE_DIR%\kernels" "%PREFIX%\share\jupyter\kernels" /E /I /F /Y
