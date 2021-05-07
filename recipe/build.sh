d58cc1f3beb2c0a82f75730947a91a57025b8e7d
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-05-07 11:13:38 +1000

    wip

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 export DisableArcade=1
 
-dotnet pack -p:TargetFrameworks=net5.0 --configuration Release --runtime linux-x64 $SRC_DIR/src/dotnet-interactive/dotnet-interactive.csproj
+dotnet pack --configuration Release --runtime linux-x64 $SRC_DIR/src/dotnet-interactive/dotnet-interactive.csproj
 dotnet tool install --framework net5.0 --add-source $SRC_DIR/src/dotnet-interactive/bin/Release --tool-path $DOTNET_TOOLS Microsoft.dotnet-interactive
 
 mkdir "$PREFIX/share/jupyter"
