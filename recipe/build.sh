2525fd936b53de8742a8d0ba5fc6f05954106866
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2022-03-09 14:06:50 +1000

    wip

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,9 @@
 
 export DisableArcade=1
 
-echo '##############################################'
-ls -la $SRC_DIR
-echo '##############################################'
-ls -la $SRC_DIR/src
-echo '##############################################'
 
-pushd $SRC_DIR/src/microsoft-dotnet-interactive
-npm ci
-npm run compile
-popd
+dotnet run -p $SRC_DIR/src/interface-generator --out-file $SRC_DIR/src/dotnet-interactive-vscode/src/interfaces/src/contracts.ts
+dotnet run -p $SRC_DIR/src/interface-generator --out-file $SRC_DIR/src/dotnet-interactive-npm/src/dotnet-interactive/contracts.ts
 
 dotnet pack --configuration Release --runtime linux-x64 $SRC_DIR/src/dotnet-interactive/dotnet-interactive.csproj
 dotnet tool install --framework net6.0 --add-source $SRC_DIR/src/dotnet-interactive/bin/Release --tool-path $DOTNET_TOOLS Microsoft.dotnet-interactive
