1d93e12082e1a331c29187709910315fca1ef63f
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2022-03-09 13:05:16 +1000

    Bump required framework to 6

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 set DisableArcade=1
 
 dotnet pack --configuration Release --runtime win-x64 %SRC_DIR%/src/dotnet-interactive/dotnet-interactive.csproj
-dotnet tool install --framework net5.0 --add-source %SRC_DIR%/src/dotnet-interactive/bin/X64/Release --tool-path %DOTNET_TOOLS% Microsoft.dotnet-interactive
+dotnet tool install --framework net6.0 --add-source %SRC_DIR%/src/dotnet-interactive/bin/X64/Release --tool-path %DOTNET_TOOLS% Microsoft.dotnet-interactive
 
 mkdir "%PREFIX%\share\jupyter"
 xcopy "%RECIPE_DIR%\kernels" "%PREFIX%\share\jupyter\kernels" /E /I /F /Y
